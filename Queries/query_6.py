36eb1e0fbefc488c8677cfe4924e17527ee640cb
Shashank-sigmoid <shashank1@sigmoidanalytics.com>
2022-06-03 21:49:54 +0530

    Code quality improved

--- Queries/query_6.py
+++ Queries/query_6.py
@@ -4,25 +4,26 @@ import pymongo as pm
 
 
 def query_6():
-    # Establishing the connection with mongoDB server
-    client = None
+
     try:
-        client = pm.MongoClient('mongodb://127.0.0.1:27017')
+        # Establishing the connection with mongoDB server
+        client = pm.MongoClient("mongodb://root:root@127.0.0.1:27017")
         print("Connection established successfully...")
+
+        # Storing the database in variable mydb
+        mydb = client["twitter_db"]
+
+        # Creating a collection
+        donations = mydb["donations"]
+
+        # Fetching data from Covid funding API
+        response = re.get(f"https://covidfunding.eiu.com/api/funds?date={date.today()}")
+        data = response.json()
+        donations.insert_many(data["funds"])
+        print("Data added successfully to the Database...")
+
     except:
         print("Error in connection")
-
-    # Storing the database in variable mydb
-    mydb = client['twitter_db']
-
-    # Creating a collection
-    donations = mydb["donations"]
-
-    # Fetching data from Covid funding API
-    response = re.get(f"https://covidfunding.eiu.com/api/funds?date={date.today()}")
-    data = response.json()
-    donations.insert_many(data['funds'])
-    print("Data added successfully to the Database...")
 
 
 if __name__ == "__main__":
--- Queries/query_8.py
+++ Queries/query_8.py
@@ -5,41 +5,45 @@ import csv
 
 
 def query_8():
-    # Establishing the connection with mongoDB server
-    client = None
+
     try:
-        client = pm.MongoClient('mongodb://127.0.0.1:27017')
+        # Establishing the connection with mongoDB server
+        client = pm.MongoClient("mongodb://127.0.0.1:27017")
         print("Connection established successfully...")
+
+        # Storing the database in variable mydb
+        mydb = client["twitter_db"]
+
+        # Creating a collection
+        global_economy = mydb["global_economy"]
+
+        # Fetching data from the nadaq API
+        qd.ApiConfig.api_key = "Ji81cMm63Vm7UxPXq6CZ"
+
+        with open("/Queries/Query_8_GDP.csv", "r") as file:
+            reader = csv.reader(file)
+            k = 0
+            for row in reader:
+                if k == 0:
+                    k = 1
+                else:
+                    data = qd.get(f"FRED/{row[2]}")
+                    data.reset_index(inplace=True)
+                    data_dict = data.to_dict("records")
+                    data_to_insert = []
+                    for record in data_dict:
+                        entry = {"Country": row[0],
+                                 "Code": row[1],
+                                 "Date": record['Date'],
+                                 "GDP": record['Value']}
+                        data_to_insert.append(entry)
+                    global_economy.insert_many(data_to_insert)
+                    print(f"Data for {row[0]} is successfully added to the database...")
+                    t.sleep(31)
+
     except:
         print("Error in connection")
 
-    # Storing the database in variable mydb
-    mydb = client['twitter_db']
 
-    # Creating a collection
-    global_economy = mydb["global_economy"]
-
-    # Fetching data from the nadaq API
-    qd.ApiConfig.api_key = 'Ji81cMm63Vm7UxPXq6CZ'
-
-    with open('/Users/shashankdey/PycharmProjects/Mock_Project/Queries/Query 8 - GDP.csv', 'r') as file:
-        reader = csv.reader(file)
-        k = 0
-        for row in reader:
-            if k == 0:
-                k = 1
-            else:
-                data = qd.get(f"FRED/{row[1]}")
-                data.reset_index(inplace=True)
-                data_dict = data.to_dict("records")
-                data_to_insert = []
-                for record in data_dict:
-                    entry = {"Country": row[0], "Date": record['Date'], "GDP": record['Value']}
-                    data_to_insert.append(entry)
-                global_economy.insert_many(data_to_insert)
-                print(f"Data for {row[0]} is added to the database...")
-                t.sleep(31)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     query_8()
