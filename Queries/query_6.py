d9d938b4d45b7300def208840b129b662f1b6e24
Shashank-sigmoid <shashank1@sigmoidanalytics.com>
2022-06-09 18:48:37 +0530

    Comments added

--- Queries/query_6.py
+++ Queries/query_6.py
@@ -7,18 +7,23 @@ def query_6():
 
     try:
         # Establishing the connection with mongoDB server
-        client = pm.MongoClient("mongodb://root:root@127.0.0.1:27017")
+        client = pm.MongoClient("mongodb://127.0.0.1:27017")
         print("Connection established successfully...")
 
-        # Storing the database in variable mydb
+        # Storing the database named "twitter_db" in variable mydb
         mydb = client["twitter_db"]
 
-        # Creating a collection
+        # Creating a collection named "donations"
         donations = mydb["donations"]
 
-        # Fetching data from Covid funding API
+        # Passing the date parameter as today's date
+        # Sending the request to the API server using the Base URL
         response = re.get(f"https://covidfunding.eiu.com/api/funds?date={date.today()}")
+
+        # Converting the response in JSON format
         data = response.json()
+
+        # Ingesting the bulk data in the collection "donations"
         donations.insert_many(data["funds"])
         print("Data added successfully to the Database...")
 
